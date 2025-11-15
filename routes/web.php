228ff357f0227186db87b0f45f78f5c354306ec2
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-11-15 00:37:42 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $nome = "Pedro";
+    $idade = 27;
+
+    $arr = [10, 20, 30, 40, 50];
+
+    $nomes = ["Matheus", "Maria", "João", "Pedro"];
+
+    return view('welcome', ["nome" => $nome, "idade" => $idade, "profissao" => "Programador", "arr" => $arr, "nomes" => $nomes]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/produtos', function () {
+    return view('products');
+});
+
